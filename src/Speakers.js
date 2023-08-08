a95c8f18d8b182bc7dcb9f44f0380c6b50c07a02
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-08-08 12:33:57 +0100

    Leveraged on useMemo to optimize filtering and sorting speakers

--- src/Speakers.js
+++ src/Speakers.js
@@ -4,6 +4,7 @@ import React, {
   useContext,
   useReducer,
   useCallback,
+  useMemo,
 } from 'react';
 import { Header } from './Header';
 import { Menu } from './Menu';
@@ -55,9 +56,9 @@ const Speakers = () => {
     });
   }, []);
 
-  const speakerListFiltered = isLoading
-    ? []
-    : speakerList
+  const newSpeakerList = useMemo(
+    () =>
+      speakerList
         .filter(
           ({ sat, sun }) =>
             (speakingSaturday && sat) || (speakingSunday && sun),
@@ -70,7 +71,11 @@ const Speakers = () => {
             return 1;
           }
           return 0;
-        });
+        }),
+    [speakingSaturday, speakingSunday, speakerList],
+  );
+
+  const speakerListFiltered = isLoading ? [] : newSpeakerList;
 
   const handleChangeSaturday = () => {
     setSpeakingSaturday(!speakingSaturday);
