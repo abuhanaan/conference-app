f7afde0a61468cf6f31e698f19971cf284509984
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-08-13 14:44:45 +0100

    Integrating ServerSide Genertated code with useContext on the Client

--- src/useSpeakerDataManager.js
+++ src/useSpeakerDataManager.js
@@ -1,11 +1,13 @@
 import SpeakersReducer from './speakersReducer';
 import axios from 'axios';
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useContext } from 'react';
+import { InitialSpeakersDataContext } from '../pages/speakers';
 
 function useSpeakerDataManager() {
+  const initialSpeakerData = useContext(InitialSpeakersDataContext);
   const [{ isLoading, speakerList }, dispatch] = useReducer(SpeakersReducer, {
-    isLoading: true,
-    speakerList: [],
+    isLoading: false,
+    speakerList: initialSpeakerData,
   });
 
   function toggleSpeakerFavorite(speakerRec) {
