e3154ecbd1852293dee988198058059ab4a5bd43
Mustopha Qomorudeen <adewunmi99@gmail.com>
2023-08-13 15:17:08 +0100

    emailValidation UI

---
+++ pages/emailValidating.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EmailValidatingForm from '../src/EmailValidatingForm';
+
+const emailValidating = () => {
+  return <EmailValidatingForm />;
+};
+
+export default emailValidating;
